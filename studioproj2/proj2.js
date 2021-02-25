b216d13972734ad5f450f29aa0db4cbf0748a418
xing801 <xing801@newschool.edu>
2021-02-25 11:54:27 -0500

    updated code

--- studioproj2/proj2.js
+++ studioproj2/proj2.js
@@ -1,16 +1,97 @@
-document.querySelectorAll(".text").forEach((item, i) => {
-	item.addEventListener("click", expand)
-})
-function expand(){
-	var button=event.currentTarget
-	document.querySelector(".dot").classList.toggle("expand")
-	console.log(button.dataset.number)
-document.querySelectorAll(".paragraph").forEach((item, i) => {
-	if(button.dataset.number==item.dataset.number) {
-		item.style.opacity=1
-	} else {
-		item.style.opacity=0
-	}
-})
+let dot = document.querySelector('.dot');
+let triggerDot = document.querySelector('#one');
+
+function toggleDot() {
+  dot.classList.toggle('expand');
 }
 
+triggerDot.addEventListener('click', toggleDot);
+
+
+
+let dot2 = document.querySelector('.dot2');
+let triggerDot2 = document.querySelector('#two');
+
+function toggleDot2() {
+  dot2.classList.toggle('expand2');
+}
+
+triggerDot2.addEventListener('click', toggleDot2);
+
+
+
+let dot3 = document.querySelector('.dot3');
+let triggerDot3 = document.querySelector('#three');
+
+function toggleDot3() {
+  dot3.classList.toggle('expand3');
+}
+
+triggerDot3.addEventListener('click', toggleDot3);
+
+
+
+let dot4 = document.querySelector('.dot4');
+let triggerDot4 = document.querySelector('#four');
+
+function toggleDot4() {
+  dot4.classList.toggle('expand4');
+}
+
+triggerDot4.addEventListener('click', toggleDot4);
+
+
+
+let dot5 = document.querySelector('.dot5');
+let triggerDot5 = document.querySelector('#five');
+
+function toggleDot5() {
+  dot5.classList.toggle('expand5');
+}
+
+triggerDot5.addEventListener('click', toggleDot5);
+
+
+
+let dot6 = document.querySelector('.dot6');
+let triggerDot6 = document.querySelector('#six');
+
+function toggleDot6() {
+  dot6.classList.toggle('expand6');
+}
+
+triggerDot6.addEventListener('click', toggleDot6);
+
+
+
+
+let dot7 = document.querySelector('.dot7');
+let triggerDot7 = document.querySelector('#seven');
+
+function toggleDot7() {
+  dot7.classList.toggle('expand7');
+}
+
+triggerDot7.addEventListener('click', toggleDot7);
+
+
+
+let dot8 = document.querySelector('.dot8');
+let triggerDot8 = document.querySelector('#eight');
+
+function toggleDot8() {
+  dot8.classList.toggle('expand8');
+}
+
+triggerDot8.addEventListener('click', toggleDot8);
+
+
+
+let dot9 = document.querySelector('.dot9');
+let triggerDot9 = document.querySelector('#nine');
+
+function toggleDot9() {
+  dot9.classList.toggle('expand9');
+}
+
+triggerDot9.addEventListener('click', toggleDot9);
