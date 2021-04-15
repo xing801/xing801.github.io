ae99be70eb1b2995463969b7002327b63fd4f2cf
xing801 <xing801@newschool.edu>
2021-04-15 17:50:43 -0400

    coded labproj4

---
+++ labproj4/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() { 
+
+	var group1 = $("group1"); 
+
+	$("#group1").click(function() { 
+		$("#one").toggle() 
+	});
+})
+
+
+$(document).ready(function() { 
+
+	var group2 = $("group2"); 
+
+	$("#group2").click(function() { 
+		$("#two").toggle() 
+	});
+})
+
+$(document).ready(function() { 
+
+	var group3 = $("group3"); 
+
+	$("#group3").click(function() { 
+		$("#three").toggle() 
+	});
+})
+
+$(document).ready(function() { 
+
+	var group4 = $("group4"); 
+
+	$("#group4").click(function() { 
+		$("#four").toggle() 
+	});
+})
+
+
+
+
