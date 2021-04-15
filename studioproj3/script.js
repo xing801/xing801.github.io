ef3fd4354c97d653630e837db8836115efcfb406
xing801 <xing801@newschool.edu>
2021-04-14 21:12:52 -0400

    updated code

--- studioproj3/script.js
+++ studioproj3/script.js
@@ -43,37 +43,6 @@ function consoleLogCovers() {
 
 function showCovers() {
   console.log("showCovers()");
-  covers.forEach((cover) => {
-    
-    // var coverName = document.createElement("h1");
-    // coverName.innerText = cover.fields.name;
-    // document.body.append(coverName);
- 
-  	var bookContainer = document.createElement("div");
-  	bookContainer.classList.add("book-container");
- 	  document.querySelector(".container").append(bookContainer);
-
-	  var coverName = document.createElement("h1");
-    coverName.innerText = cover.fields.tag;
-    bookContainer.append(coverName);
-
-    var coverName = document.createElement("h2");
-    coverName.innerText = cover.fields.name;
-    bookContainer.append(coverName);
-
-    var coverName = document.createElement("h3");
-    coverName.innerText = cover.fields.author;
-    bookContainer.append(coverName);
-
-    var coverName = document.createElement("p");
-    // coverInfo.classList.add("cover-info");
-    coverName.innerText = cover.fields.info;
-    bookContainer.append(coverName);
-
-    var coverImage = document.createElement('img');
-    coverImage.src = cover.fields.image[0].url;
-    bookContainer.append(coverImage);
-
     let tagOne = covers.filter(cover => cover.fields.tag == '1');
     let tagTwo = covers.filter(cover => cover.fields.tag == '2');
     let tagThree = covers.filter(cover => cover.fields.tag == '3');
@@ -81,12 +50,192 @@ function showCovers() {
     let tagFive = covers.filter(cover => cover.fields.tag == '5');
     let tagSix = covers.filter(cover => cover.fields.tag == '6');
 
-    bookContainer.addEventListener("click", function() {
-        coverInfo.classList.toggle("active");
+    var box1 = document.querySelector("#group1");
+    box1.addEventListener("click", function() {
+      box1.innerHTML = ``;
+      tagOne.forEach((cover) => {
+        var coverName = document.createElement("h2");
+        coverName.innerText = cover.fields.name;
+        box1.append(coverName);
+
+        var coverName = document.createElement("h3");
+        coverName.innerText = cover.fields.author;
+        box1.append(coverName);
+
+        var coverName = document.createElement("p");
+        coverName.innerText = cover.fields.info;
+        box1.append(coverName);
+
+        var coverImage = document.createElement('img');
+        coverImage.src = cover.fields.image[0].url;
+        box1.append(coverImage);
+
+      })
     })
 
-  });
-}
+    var box2 = document.querySelector("#group2");
+    box2.addEventListener("click", function() {
+      box2.innerHTML = ``;
+      tagTwo.forEach((cover) => {
+        var coverName = document.createElement("h2");
+        coverName.innerText = cover.fields.name;
+        box2.append(coverName);
+
+        var coverName = document.createElement("h3");
+        coverName.innerText = cover.fields.author;
+        box2.append(coverName);
+
+        var coverName = document.createElement("p");
+        coverName.innerText = cover.fields.info;
+        box2.append(coverName);
+
+        var coverImage = document.createElement('img');
+        coverImage.src = cover.fields.image[0].url;
+        box2.append(coverImage);
+
+      })
+    })
+
+    var box3 = document.querySelector("#group3");
+    box3.addEventListener("click", function() {
+      box3.innerHTML = ``;
+      tagThree.forEach((cover) => {
+        var coverName = document.createElement("h2");
+        coverName.innerText = cover.fields.name;
+        box3.append(coverName);
+
+        var coverName = document.createElement("h3");
+        coverName.innerText = cover.fields.author;
+        box3.append(coverName);
+
+        var coverName = document.createElement("p");
+        coverName.innerText = cover.fields.info;
+        box3.append(coverName);
+
+        var coverImage = document.createElement('img');
+        coverImage.src = cover.fields.image[0].url;
+        box3.append(coverImage);
+
+      })
+    })
+
+    var box4 = document.querySelector("#group4");
+    box4.addEventListener("click", function() {
+      box4.innerHTML = ``;
+      tagFour.forEach((cover) => {
+        var coverName = document.createElement("h2");
+        coverName.innerText = cover.fields.name;
+        box4.append(coverName);
+
+        var coverName = document.createElement("h3");
+        coverName.innerText = cover.fields.author;
+        box4.append(coverName);
+
+        var coverName = document.createElement("p");
+        coverName.innerText = cover.fields.info;
+        box4.append(coverName);
+
+        var coverImage = document.createElement('img');
+        coverImage.src = cover.fields.image[0].url;
+        box4.append(coverImage);
+
+      })
+    })
+
+    var box5 = document.querySelector("#group5");
+    box5.addEventListener("click", function() {
+      box5.innerHTML = ``;
+      tagFive.forEach((cover) => {
+        var coverName = document.createElement("h2");
+        coverName.innerText = cover.fields.name;
+        box5.append(coverName);
+
+        var coverName = document.createElement("h3");
+        coverName.innerText = cover.fields.author;
+        box5.append(coverName);
+
+        var coverName = document.createElement("p");
+        coverName.innerText = cover.fields.info;
+        box5.append(coverName);
+
+        var coverImage = document.createElement('img');
+        coverImage.src = cover.fields.image[0].url;
+        box5.append(coverImage);
+
+      })
+    })
+
+    var box6 = document.querySelector("#group6");
+    box6.addEventListener("click", function() {
+      box6.innerHTML = ``;
+      tagSix.forEach((cover) => {
+        var coverName = document.createElement("h2");
+        coverName.innerText = cover.fields.name;
+        box6.append(coverName);
+
+        var coverName = document.createElement("h3");
+        coverName.innerText = cover.fields.author;
+        box6.append(coverName);
+
+        var coverName = document.createElement("p");
+        coverName.innerText = cover.fields.info;
+        box6.append(coverName);
+
+        var coverImage = document.createElement('img');
+        coverImage.src = cover.fields.image[0].url;
+        box6.append(coverImage);
+
+      })
+    })
+};
+
+
+// var subbox1 = document.querySelector("#group1");
+//     subbox1.addEventListener("click", function() {
+//       subbox1.innerHTML = ``;
+//       tagOne.forEach((cover) => {
+
+//         var coverName = document.createElement("h3");
+//         coverName.innerText = cover.fields.author;
+//         document.body.append(coverName);
+
+//         var coverName = document.createElement("p");
+//         coverName.innerText = cover.fields.info;
+//         document.body.append(coverName);
+
+    
+    // var coverName = document.createElement("h1");
+    // coverName.innerText = cover.fields.name;
+    // document.body.append(coverName);
+
+	  // var coverName = document.createElement("h1");
+   //  coverName.innerText = cover.fields.tag;
+   //  bookContainer.append(coverName);
+
+   //  var coverName = document.createElement("h2");
+   //  coverName.innerText = cover.fields.name;
+   //  bookContainer.append(coverName);
+
+   //  var coverName = document.createElement("h3");
+   //  coverName.innerText = cover.fields.author;
+   //  bookContainer.append(coverName);
+
+   //  var coverName = document.createElement("p");
+   //  // coverInfo.classList.add("cover-info");
+   //  coverName.innerText = cover.fields.info;
+   //  bookContainer.append(coverName);
+
+   //  // var coverInfo = document.createElement("p");
+
+   //  var coverImage = document.createElement('img');
+   //  coverImage.src = cover.fields.image[0].url;
+   //  bookContainer.append(coverImage);
+
+
+   //  bookContainer.addEventListener("click", function() {
+   //      coverInfo.classList.toggle("active");
+   //  })
+ 
 
 
 var cube = document.getElementById('cube');
