1b770acfddacee6b5b61a6ed14050e1e69dfa5fb
xing801 <xing801@newschool.edu>
2021-05-06 08:36:36 -0400

    studioproj4

---
+++ proj4/script.js
@@ -0,0 +1,272 @@
+$(document).ready(function() {   
+
+	var one = $("one"); 
+
+	$(".one").click(function() { 
+		$(".two").show()
+		$(".six").show()
+	});
+})
+
+$(document).ready(function() {   
+
+	var group1 = $("group1"); 
+
+	$("#group1").click(function() { 
+		$("#group1").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group2 = $("group2"); 
+
+	$("#group2").click(function() { 
+		$("#group2").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group3 = $("group3"); 
+
+	$("#group3").click(function() { 
+		$("#group3").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group4 = $("group4"); 
+
+	$("#group4").click(function() { 
+		$("#group4").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var two = $("two"); 
+
+	$(".two").click(function() { 
+		$(".three").show()
+		$(".seven").show()
+	});
+})
+
+$(document).ready(function() {   
+
+	var group5 = $("group5"); 
+
+	$("#group5").click(function() { 
+		$("#group5").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group6 = $("group6"); 
+
+	$("#group6").click(function() { 
+		$("#group6").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group7 = $("group7"); 
+
+	$("#group7").click(function() { 
+		$("#group7").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group8 = $("group8"); 
+
+	$("#group8").click(function() { 
+		$("#group8").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var three = $("three"); 
+
+	$(".three").click(function() { 
+		$(".four").show()
+		$(".eight").show()
+	});
+})
+
+$(document).ready(function() {   
+
+	var group9 = $("group9"); 
+
+	$("#group9").click(function() { 
+		$("#group9").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group10 = $("group10"); 
+
+	$("#group10").click(function() { 
+		$("#group10").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group11 = $("group11"); 
+
+	$("#group11").click(function() { 
+		$("#group11").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group12 = $("group12"); 
+
+	$("#group12").click(function() { 
+		$("#group12").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+
+$(document).ready(function() {   
+
+	var four = $("four"); 
+
+	$(".four").click(function() { 
+		$(".five").show()
+	});
+})
+
+$(document).ready(function() {   
+
+	var group13 = $("group13"); 
+
+	$("#group13").click(function() { 
+		$("#group13").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group14 = $("group14"); 
+
+	$("#group14").click(function() { 
+		$("#group14").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group15 = $("group15"); 
+
+	$("#group15").click(function() { 
+		$("#group15").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group16 = $("group16"); 
+
+	$("#group16").click(function() { 
+		$("#group16").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group17 = $("group17"); 
+
+	$("#group17").click(function() { 
+		$("#group17").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group18 = $("group18"); 
+
+	$("#group18").click(function() { 
+		$("#group18").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group19 = $("group19"); 
+
+	$("#group19").click(function() { 
+		$("#group19").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var group20 = $("group20"); 
+
+	$("#group20").click(function() { 
+		$("#group20").show()
+		 $(this).css("opacity", "1")
+	});
+})
+
+$(document).ready(function() {   
+
+	var nine = $("nine"); 
+
+	$(".five").click(function() { 
+		$(".nine").show()
+	});
+})
+
+var quotes = [
+	"You love perfecting the details of life, applying creativity and rationality to everything you do. Your inner world is often a private, complex one.",
+	"You are a flexible thinker who enjoys taking an unconventional approach to many aspects of life. You often seek out unlikely paths, mixing willingness to experiment with personal creativity.",
+	"You are decisive and love momentum and accomplishment. You gather information to construct your creative visions but rarely hesitate for long before acting on them.",
+	"You tend to be bold and creative, deconstructing and rebuilding ideas with great mental agility. You pursue your goals vigorously despite any resistance you might encounter.",
+	"You tend to approach life with deep thoughtfulness and imagination. Your inner vision, personal values, and a quiet, principled version of humanism guide you in all things.",
+	"You are someone who tends to be quiet, open-minded, and imaginative, and you apply a caring and creative approach to everything you do.",
+	"You are warm and love helping others. You tend to have strong ideas and values. You back your perspective with the creative energy to achieve your goals.",
+	"You tend to embrace big ideas and actions that reflect your sense of hope and goodwill toward others. Your vibrant energy can flow in many directions.",
+	"You tend to be reserved yet willful, with a rational outlook on life. You compose your actions carefully and carry them out with methodical purpose.",
+	"You tend to be warm and unassuming in your own steady way. You are efficient and responsible, giving careful attention to practical details in your daily lives.",
+	"You possess great fortitude, emphatically following your own sensible judgment. You often serve as a stabilizing force among others, able to offer solid direction amid adversity.",
+	"You are attentive and people-focused, and you enjoy taking part in your social community. Your achievements are guided by decisive values, and you willingly offer guidance to others.",
+	"You tend to have an individualistic mindset, pursuing goals without needing much external connection. You engage in life with inquisitiveness and personal skill, varying your approach as needed.",
+	"You tend to have open minds, approaching life, new experiences, and people with grounded warmth. Your ability to stay in the moment helps you uncover exciting potentials.",
+	"You tend to be energetic and action-oriented, deftly navigating whatever is in front of you. You love uncovering life’s opportunities, whether socializing with others or in more personal pursuits.",
+	"You love vibrant experiences, engaging in life eagerly and taking pleasure in discovering the unknown. You can be very social, often encouraging others into shared activities.",
+] 
+
+function getQuote() {
+	var randomNumber = Math.floor(Math.random() * quotes.length);
+	document.getElementById('newQuoteSection').innerHTML = quotes[randomNumber];
+}
